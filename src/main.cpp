a1efe3a0ab3daa180e61a604cfa0d30edf5f62e5
Noah Hdez <noah.hernandez101@alu.ulpgc.es>
2021-10-27 13:55:24 +0100

    Fix audio lag

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
 #include <chrono>
+
+#include <Windows.h>
+#include <WinUser.h>
 
 #include "Memory.h"
 
 int main(int argc, char*argv[])
 {
     sf::RenderWindow window(sf::VideoMode(160, 144), "BoiBoy");
+
+    sf::WindowHandle hwnd = window.getSystemHandle();
+
+    HMENU hMenuBar = CreateMenu();
+    HMENU hFile = CreateMenu();
+    std::wstring sFile = L"File";
+    LPCWSTR shFile = sFile.c_str();
+    AppendMenu(hMenuBar, MF_POPUP, (UINT_PTR)hFile, shFile);
+    std::wstring sExit = L"Exit";
+    LPCWSTR shExit = sExit.c_str();
+    AppendMenu(hFile, MF_STRING, 1, shExit);
+    SetMenu(hwnd, hMenuBar);
+
     sf::Vector2u size(480, 432);
     window.setSize(size);
     sf::Texture pixels;
@@ -16,8 +33,7 @@ int main(int argc, char*argv[])
     screen.setTexture(pixels);
 
     begin:
-    //Cartridge cart(argv[1]);
-    Cartridge cart("../BoiBoyCopia/Zelda.gb");
+    Cartridge cart(argv[1]);
 
     BoiBoy boi(&cart);
 
@@ -42,134 +58,6 @@ int main(int argc, char*argv[])
                 nextInst = false;
         }
 
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::KeyPressed) {
-#pragma region joypad
-                if (event.key.code == sf::Keyboard::D) {
-                    boi.controllerInput &= ~0x01;
-                }
-                if (event.key.code == sf::Keyboard::A) {
-                    boi.controllerInput &= ~0x02;
-                }
-                if (event.key.code == sf::Keyboard::W) {
-                    boi.controllerInput &= ~0x04;
-                }
-                if (event.key.code == sf::Keyboard::S) {
-                    boi.controllerInput &= ~0x08;
-                }
-                if (event.key.code == sf::Keyboard::M) {
-                    boi.controllerInput &= ~0x10;
-                }
-                if (event.key.code == sf::Keyboard::N) {
-                    boi.controllerInput &= ~0x20;
-                }
-                if (event.key.code == sf::Keyboard::Q) {
-                    boi.controllerInput &= ~0x40;
-                }
-                if (event.key.code == sf::Keyboard::Enter) {
-                    boi.controllerInput &= ~0x80;
-                }
-#pragma endregion
-                if (event.key.code == sf::Keyboard::Escape) {
-                    window.close();
-                }
-                if (event.key.code == sf::Keyboard::I) {
-                    goto begin;
-                }
-                if (event.key.code == sf::Keyboard::F) {
-                    if (debugFrameMode) {
-                        std::cout << "Frame mode out" << std::endl;
-                        debugFrameMode = false;
-                        nextFrame = true;
-                        nextInst = true;
-                    }
-                    else {
-                        std::cout << "Frame mode on" << std::endl;
-                        debugFrameMode = true;
-                        nextFrame = false;
-                        nextInst = false;
-                    }
-                }
-                if (event.key.code == sf::Keyboard::P) {
-                    if (debugInstMode) {
-                        std::cout << "Individual opcode mode out" << std::endl;
-                        debugInstMode = false;
-                        nextInst = true;
-                    }
-                    else {
-                        std::cout << "Individual opcode mode on" << std::endl;
-                        debugInstMode = true;
-                        nextInst = false;
-                    }
-                }
-                if (event.key.code == sf::Keyboard::O) {
-                    if (debugCpu) {
-                        std::cout << "Cpu debug mode out" << std::endl;
-                        debugCpu = false;
-                        boi.cpu.debug = false;
-                        boi.cpu.debugOp = false;
-                    }
-                    else {
-                        std::cout << "Cpu debug mode on" << std::endl;
-                        debugCpu = true;
-                        boi.cpu.debug = true;
-                        boi.cpu.debugOp = true;
-                    }
-                }
-                if (event.key.code == sf::Keyboard::Space) {
-                    if (debugInstMode)
-                        nextInst = true;
-                    else if (debugFrameMode) {
-                        nextFrame = true;
-                        nextInst = true;
-                    }
-                }
-                // Channels enables
-                if (event.key.code == sf::Keyboard::Num1) {
-                    boi.spu.channel1 = !boi.spu.channel1;
-                }
-                if (event.key.code == sf::Keyboard::Num2) {
-                    boi.spu.channel2 = !boi.spu.channel2;
-                }
-                if (event.key.code == sf::Keyboard::Num3) {
-                    boi.spu.channel3 = !boi.spu.channel3;
-                }
-                if (event.key.code == sf::Keyboard::Num4) {
-                    boi.spu.channel4 = !boi.spu.channel4;
-                }
-            }
-            if (event.type == sf::Event::KeyReleased) {
-                if (event.key.code == sf::Keyboard::D) {
-                    boi.controllerInput |= 0x01;
-                }
-                if (event.key.code == sf::Keyboard::A) {
-                    boi.controllerInput |= 0x02;
-                }
-                if (event.key.code == sf::Keyboard::W) {
-                    boi.controllerInput |= 0x04;
-                }
-                if (event.key.code == sf::Keyboard::S) {
-                    boi.controllerInput |= 0x08;
-                }
-                if (event.key.code == sf::Keyboard::M) {
-                    boi.controllerInput |= 0x10;
-                }
-                if (event.key.code == sf::Keyboard::N) {
-                    boi.controllerInput |= 0x20;
-                }
-                if (event.key.code == sf::Keyboard::Q) {
-                    boi.controllerInput |= 0x40;
-                }
-                if (event.key.code == sf::Keyboard::Enter) {
-                    boi.controllerInput |= 0x80;
-                }
-            }
-        }
-
         if (cycles >= 17556 /*boi.ppu.frameComplete*/ && nextFrame) {
 
             //while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < 16.67) {
@@ -177,6 +65,134 @@ int main(int argc, char*argv[])
             //}
             frames++;
             //std::cout << frames << std::endl;
+
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+                if (event.type == sf::Event::KeyPressed) {
+#pragma region joypad
+                    if (event.key.code == sf::Keyboard::D) {
+                        boi.controllerInput &= ~0x01;
+                    }
+                    if (event.key.code == sf::Keyboard::A) {
+                        boi.controllerInput &= ~0x02;
+                    }
+                    if (event.key.code == sf::Keyboard::W) {
+                        boi.controllerInput &= ~0x04;
+                    }
+                    if (event.key.code == sf::Keyboard::S) {
+                        boi.controllerInput &= ~0x08;
+                    }
+                    if (event.key.code == sf::Keyboard::M) {
+                        boi.controllerInput &= ~0x10;
+                    }
+                    if (event.key.code == sf::Keyboard::N) {
+                        boi.controllerInput &= ~0x20;
+                    }
+                    if (event.key.code == sf::Keyboard::Q) {
+                        boi.controllerInput &= ~0x40;
+                    }
+                    if (event.key.code == sf::Keyboard::Enter) {
+                        boi.controllerInput &= ~0x80;
+                    }
+#pragma endregion
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        window.close();
+                    }
+                    if (event.key.code == sf::Keyboard::I) {
+                        goto begin;
+                    }
+                    if (event.key.code == sf::Keyboard::F) {
+                        if (debugFrameMode) {
+                            std::cout << "Frame mode out" << std::endl;
+                            debugFrameMode = false;
+                            nextFrame = true;
+                            nextInst = true;
+                        }
+                        else {
+                            std::cout << "Frame mode on" << std::endl;
+                            debugFrameMode = true;
+                            nextFrame = false;
+                            nextInst = false;
+                        }
+                    }
+                    if (event.key.code == sf::Keyboard::P) {
+                        if (debugInstMode) {
+                            std::cout << "Individual opcode mode out" << std::endl;
+                            debugInstMode = false;
+                            nextInst = true;
+                        }
+                        else {
+                            std::cout << "Individual opcode mode on" << std::endl;
+                            debugInstMode = true;
+                            nextInst = false;
+                        }
+                    }
+                    if (event.key.code == sf::Keyboard::O) {
+                        if (debugCpu) {
+                            std::cout << "Cpu debug mode out" << std::endl;
+                            debugCpu = false;
+                            boi.cpu.debug = false;
+                            boi.cpu.debugOp = false;
+                        }
+                        else {
+                            std::cout << "Cpu debug mode on" << std::endl;
+                            debugCpu = true;
+                            boi.cpu.debug = true;
+                            boi.cpu.debugOp = true;
+                        }
+                    }
+                    if (event.key.code == sf::Keyboard::Space) {
+                        if (debugInstMode)
+                            nextInst = true;
+                        else if (debugFrameMode) {
+                            nextFrame = true;
+                            nextInst = true;
+                        }
+                    }
+                    // Channels enables
+                    if (event.key.code == sf::Keyboard::Num1) {
+                        boi.spu.channel1 = !boi.spu.channel1;
+                    }
+                    if (event.key.code == sf::Keyboard::Num2) {
+                        boi.spu.channel2 = !boi.spu.channel2;
+                    }
+                    if (event.key.code == sf::Keyboard::Num3) {
+                        boi.spu.channel3 = !boi.spu.channel3;
+                    }
+                    if (event.key.code == sf::Keyboard::Num4) {
+                        boi.spu.channel4 = !boi.spu.channel4;
+                    }
+                }
+                if (event.type == sf::Event::KeyReleased) {
+                    if (event.key.code == sf::Keyboard::D) {
+                        boi.controllerInput |= 0x01;
+                    }
+                    if (event.key.code == sf::Keyboard::A) {
+                        boi.controllerInput |= 0x02;
+                    }
+                    if (event.key.code == sf::Keyboard::W) {
+                        boi.controllerInput |= 0x04;
+                    }
+                    if (event.key.code == sf::Keyboard::S) {
+                        boi.controllerInput |= 0x08;
+                    }
+                    if (event.key.code == sf::Keyboard::M) {
+                        boi.controllerInput |= 0x10;
+                    }
+                    if (event.key.code == sf::Keyboard::N) {
+                        boi.controllerInput |= 0x20;
+                    }
+                    if (event.key.code == sf::Keyboard::Q) {
+                        boi.controllerInput |= 0x40;
+                    }
+                    if (event.key.code == sf::Keyboard::Enter) {
+                        boi.controllerInput |= 0x80;
+                    }
+                }
+            }
 
             window.clear();
             pixels.update(boi.ppu.GetScreen());
